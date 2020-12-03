67cba865f84331edca88cabe3454dc4d81b75f23
hyess210 <55439512+hyess210@users.noreply.github.com>
2020-12-03 20:34:43 +0900

    [FIX] Clendar styling
    [FIX] - 스타일링 수정 (date input 스타일링, 폰트 추가)

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+const setTextContent = (element, content) => {
+  document.querySelector(element).textContent = content;
+};
+
+let calculatedDate = 0;
+
 const DateInit = {
   monthList: [
     '1월',
@@ -33,27 +39,26 @@ const DateInit = {
     this.activeDate = d;
     return d;
   },
-  addZero: (num) => (num < 10 ? '0' + num : num),
-  activeDTag: null,
+  formatNumber: (num) => (num < 10 ? '0' + num : num),
+  selectedDateTag: null,
+  selectedDate = new Date(),
   getIndex: function (node) {
     let index = 0;
     while ((node = node.previousElementSibling)) {
       index++;
     }
     return index;
-  }
+  },
 };
-
 
 const $calendarBody = document.querySelector('.calendarBody');
 const $btnNext = document.querySelector('.cal-buttonStyle.next');
 const $btnPrev = document.querySelector('.cal-buttonStyle.prev');
 
 const getCalendarDate = (date, day) => {
-    document.querySelector('.calendarDate').textContent = date;
-    document.querySelector('.calendarDay').textContent = 
-    DateInit.dayNameList[day];
-}
+  setTextContent('.calendarDate', date);
+  setTextContent('.calendarDay', DateInit.dayNameList[day]);
+};
 
 const getCalendarYearMonth = (fullDate) => {
   let year = fullDate.getFullYear();
@@ -71,6 +76,8 @@ const getCalendarYearMonth = (fullDate) => {
 
   document.querySelector('.calendarMonth').textContent =
     DateInit.monthList[month];
+  document.querySelector('.calendarMonth-info').textContent =
+    DateInit.monthList[month];
   document.querySelector('.calendarYear').textContent = year;
 
   let tableTag = '';
@@ -84,15 +91,21 @@ const getCalendarYearMonth = (fullDate) => {
         startCount = 1;
       }
       if (!startCount) {
-        tableTag += '<td>';
+        tableTag += '<td><span>';
       } else {
         let fullDate =
           year +
           '.' +
-          DateInit.addZero(month + 1) +
+          DateInit.formatNumber(month + 1) +
           '.' +
-          DateInit.addZero(dayCount + 1);
-        tableTag += '<td class="day';
+          DateInit.formatNumber(dayCount + 1);
+          console.log(DateInit.selectedDate);
+
+        tableTag += `<td> 
+        <span onclick={dateClick(${dayCount + 1}),
+        ${month}} 
+        class="day`;
+
         tableTag +=
           writeToday && writeToday === dayCount + 1 ? ' today" ' : '"';
         tableTag += ` data-date="${dayCount + 1}" data-fdate="${fullDate}">`;
@@ -101,7 +114,7 @@ const getCalendarYearMonth = (fullDate) => {
       if (dayCount === endDay.getDate()) {
         startCount = 0;
       }
-      tableTag += '</td>';
+      tableTag += '</span></td>';
     }
     tableTag += '</tr>';
   }
@@ -109,18 +122,27 @@ const getCalendarYearMonth = (fullDate) => {
 };
 
 $calendarBody.addEventListener('click', (e) => {
-    if (e.target.classList.contains('day')) {
-      if (DateInit.activeDTag) {
-        DateInit.activeDTag.classList.remove('day-active');
-      }
-      let day = Number(e.target.textContent);
-      getCalendarDate(day, e.target.cellIndex);
-      e.target.classList.add('day-active');
-      DateInit.activeDTag = e.target;
-      DateInit.activeDate.setDate(day);
-      reloadTodo();
+  if (e.target.classList.contains('day')) {
+    if (DateInit.selectedDateTag) {
+      DateInit.selectedDateTag.classList.remove('day-active');
     }
-  });
+    let day = Number(e.target.textContent);
+    getCalendarDate(day, e.target.cellIndex);
+    e.target.classList.add('day-active');
+    DateInit.selectedDateTag = e.target;
+    DateInit.selectedDate = e.target;
+    DateInit.activeDate.setDate(day);
+
+    getCalendarYearMonth(DateInit.today);
+    getCalendarDate(DateInit.today.getDate(), DateInit.today.getDay());
+  }
+});
 
 getCalendarYearMonth(DateInit.today);
 getCalendarDate(DateInit.today.getDate(), DateInit.today.getDay());
+
+const dateClick = (clickedDate, month) => {
+  calculatedDate++;
+  setTextContent('#calculated-date', calculatedDate + '일');
+  console.log(clickedDate);
+};
